e0920596efb9d8cf2a1bf517917291ad4e8774c2
lilei <9510339+qphys@user.noreply.gitee.com>
2021-08-02 16:41:13 +0800

    almost finish template files 21.08.02

---
+++ temptex/msclean.bat
@@ -0,0 +1,4 @@
+REM ����Ŀ¼
+del /F *.log *.aux *.ist *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.lot *.lof *.blg *.out *.bak *.ps *.vrb
+
+del /S *.aux *.blg *.log *.bak *.nav *.snm *.gz(busy) *.vrb
